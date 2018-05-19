ddc508ba53e558adb776340a1cab8c9762a0fe00
Stuart Schafer <stuart_schafer@hotmail.com>
2018-05-19 09:29:22 -0400

    Getting it there

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,13 +8,13 @@ INSERT INTO USERS (name, email, password, createdAt, updatedAt) VALUES ("Stuart"
 
 -- Inserting info for the Jobs table for UserId 1
 INSERT INTO JOBS (date_applied, position, company, location, description, id_number, link, posted_from, interest_level, notes, createdAt, updatedAt, UserId)
-VALUES ("2018-05-01", "President", "Schafer Enterprise", "Raleigh, NC", "Do everything", "111", "www.schaferenterprise.com", "LinkedIn", 5, "Well, I guess you run this.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1);
+VALUES ("2018-05-01", "President", "Schafer Enterprise", "Raleigh, NC", "Do everything", "111", "www.cnn.com", "LinkedIn", 5, "Well, I guess you run this.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1);
 
 INSERT INTO JOBS (date_applied, position, company, location, description, id_number, link, posted_from, interest_level, notes, createdAt, updatedAt, UserId)
-VALUES ("2018-05-02", "Vice President", "Schafer Industries", "Charlotte, NC", "Do everything almost everything", "12345", "www.schaferindustries.com", "Glassdoor", 3, "You kinda run things around here.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1);
+VALUES ("2018-05-02", "Vice President", "Schafer Industries", "Charlotte, NC", "Do everything almost everything", "12345", "www.yahoo.com", "Glassdoor", 3, "You kinda run things around here.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1);
 
 INSERT INTO JOBS (date_applied, position, company, location, description, id_number, link, posted_from, interest_level, notes, createdAt, updatedAt, UserId)
-VALUES ("2018-05-03", "The Man", "Schafer Incorporated", "Chapel Hill, NC", "Do whatever you want", "85676", "www.schaferincorporated.com", "Zip Recruiter", 10, "Coolest job for the coolest kid.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1);
+VALUES ("2018-05-03", "The Man", "Schafer Incorporated", "Chapel Hill, NC", "Do whatever you want", "85676", "www.youtube.com", "Zip Recruiter", 10, "Coolest job for the coolest kid.", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1);
 
 
 
