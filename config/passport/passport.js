7347aa8528814939e77c93c333ef3325ba23c45d
Stuart Schafer <stuart_schafer@hotmail.com>
2018-06-20 01:20:18 -0400

    Trying to get hti working again on heroku

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -49,7 +49,7 @@ module.exports = function(passport, user) {
 					hashSecurity(question2, answer2);
 
 					function hashSecurity(question, answer) {
-						let num = hMath.floor(Math.random() * 9) + 1;
+						let num = Math.floor(Math.random() * 9) + 1;
 						let allChars = ["%", "J", "V", "(", "O", "f", "N", "z", "r", "y", "1", "R", "l", "Z", "0", "H", "u", " ", "T", "@", "v", "e", ">", "k", "]", "2", "n", "C", "g", "`", "5", "L", "{", "?", "~", "d", "x", "4", "j", "&", "<", "o", "c", "Q", "B", "K", "E", "w", "h", "i", "b", "Y", "3", "W", "U", "7", ")", "F", "p", "}", "$", "*", "#", "M", "9", "m", "a", "8", "X", "i", "A", "q", "S", "t", "s", "I", "6", "[", "P", "^", ":", ";", "G", "D", ",", "%", "J", "V", "(", "O", "f", "N", "z", "r", "y", "1", "R", "l", "Z", "0", "H", "u", "T", "@", "v", "e", ">", "k", "]", "2", "n", "C", "g", "`", "5", "L", "{", "?", "~", "d", "x", "4", "j", "&", "<", "o", "c", "Q", "B", "K", "E", "w", "h", "i", "b", "Y", "3", "W", "U", "7", ")", "F", "p", "}", "$", "*", "#", "M", "9", "m", "a", "8", "X", "i", "A", "q", "S", "t", "s", "I", "6", "[", "P", "^", ":", ";", "G", "D", ","];
 						let count = "";
 						if (question.length < 10) {
